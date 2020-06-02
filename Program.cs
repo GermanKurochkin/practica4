73d101b734beb8d669ffcd67ec99979329f0f66c
Герман Курочкин <ggkurochkin@edu.hse.ru>
2020-06-02 12:15:44 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace practica4
+{
+    class Program
+    {
+        static bool Square(int num)
+        {
+            return num>0&& Math.Sqrt(num) - Math.Round(Math.Sqrt(num)) == 0;
+        }
+        static int InputNum()
+        {
+            int num;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out num);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+            } while (!ok || num < 1 );
+
+            return num;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = InputNum();
+            for(int i=1;i<=n;i++)
+            {
+                for (int j = 1; j <= Math.Sqrt(i); j++)
+                {
+                    if (Square(i - j * j))
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
